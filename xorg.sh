63441c2d058440b0a14c0918b02d94471e5e0109
Victorien Elvinger <victorien@elvinger.fr>
2017-02-28 21:28:57 +0100

    fix(xorg): prevent $Xauthority set if it exists
    Display managers source /etc/profile after executing X server.
    As a result, $Xauthoriity is set to the wrong location.
    
    $Xauthority is set if it is not already set and the file
    ~/.Xauthority does not exist.
    You need to remove this file if you want to use the XDG fix.

--- xorg.sh
+++ xorg.sh
@@ -4,7 +4,7 @@
 # Copyright (c) 2017 Victorien Elvinger
 #
 
-command -v 'xinit' > /dev/null 2>&1 &&
+test -z "$XAUTHORITY" && ! test -f $HOME'/.Xauthority' &&
     export XAUTHORITY=$XDG_RUNTIME_DIR'/Xauthority'
-        # XDG fix
+        # XDG fix if Xauthority does not already exist
 
